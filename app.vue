78d91ee83e7884e1fbad4f67122a8783b25c4889
Frank Spee <frank.spee@bitfactory.nl>
2023-12-27 12:39:32 +0100

    feat: :tada: Initial commit test Nuxt 3 with Ionic module

---
+++ app.vue
@@ -0,0 +1,11 @@
+<!-- <template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template> -->
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // css: [
+  //   "@/theme/variables.css",
+  // ],
+  devtools: { 
+    enabled: true,
+  },
+  // NOTE: need to set this because of a bug in Nuxt / Nuxt Ionic module. See link below.
+  // https://github.com/nuxt-modules/ionic/issues/484
+  experimental: {
+    payloadExtraction: false
+  },
+  modules: [
+    '@nuxtjs/ionic',
+  ],
+  // ssr: false,
+  // typescript: {
+  //   shim: false,
+  // },
+})
